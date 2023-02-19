aec260cc5d21dbdd4a5a0e0ee87f2251e127776c
jennylewis45 <121876323+jennylewis45@users.noreply.github.com>
2023-02-19 21:41:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,1100 @@
+// alert("Hello World")
+// var a=20;
+// var b=10;
+// var c=a+b;
+// alert(c)
+// var a=20;
+// var b=10;
+// var c=a+b;
+// console.log(c)
+
+// var x= 20;
+// var y= "ali";
+// var z= y+z;
+//   alert(z);
+
+
+//  var x= 20;
+//   var y= "10";
+//   var z= y+x;
+//     alert(z);
+
+   
+//       var x= 20;
+//   var y= "2";
+//      var z= x*y;
+//        alert(z);
+
+//        var x= 20;
+//        var y= "2";
+//           var z= x/y
+//             alert(z);
+
+// var a=3
+// var b=7
+// var c=2
+// var d=(b-a) *c
+// alert(d);
+
+
+// var a=2
+// var b=a++;
+// alert(b);
+
+// alert(a);
+
+// var a=2
+// var b=++a;
+// alert(b);
+
+// var x=10
+// var y=x++ + ++x -x +x++-++x+x
+// alert(y);
+
+
+// var a=prompt("Enter your age","your age is");
+// alert(a);
+
+// var a=+prompt("Enter your value","your value is");
+// var b = a+45
+// console.log("The output is" , ""+b)
+
+// var spec=prompt("your species?","human");
+
+// var question = "Your species?";
+//  var defaultAnswer = "human";
+//  var spec = prompt(question, defaultAnswer);
+
+//  var numberOfhats = prompt("How many hats?");
+//  var tooManyhats =2 + 10;
+
+// var x="enter value";
+// var y="your value is"
+// var a=+prompt(x,y);
+// var b= a+10;
+// document.write("the output is" +"" +b)
+
+// var age=+prompt("enter your age");
+// if(age==14){
+//     alert("your age is equal")
+
+// }
+// else if(age <14){
+//     alert("your age is less"  )
+// }
+
+
+
+// else{
+//     alert("you age is more")
+// }
+
+// var age=+prompt("enter your age");
+// var gender=prompt("enter your gender")
+// if("age > 18 $$ gender==female"){
+//     alert("you are allowed")
+// }
+// else{
+//     alert("yoy are not allowed")
+// }
+
+// var a="5";
+// if (a==5){
+//     alert("true")
+
+// }
+// else{
+//     alert( "false")
+//        ( "false")
+//     }
+
+// var x=5;
+// var y=10;
+// vara=30;
+// var b=15;
+// if("x-y===a-b"){
+//     alert("correct")
+// }
+
+// else{
+//     alert("InCorrect")
+// }
+    
+// var a=5;
+// if(a !== 10){
+//     document.write("Correct")
+// }
+// else{
+
+// document.write("Incorrect")
+// }
+
+//  if (weight > 300 && time < 6) {
+//      alert("Come to our tryout!");
+//     }
+//      else {
+//      alert("Come to our cookout!");
+//      }
+
+//       if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//      alert("Come to our tryout!");
+//      }
+//      else {
+//          alert("Come to our cookout!");
+//          }
+
+// var percent= prompt("enter your percent");
+// if(percent >=80 && percent <=100){
+// alert("A+")
+// }
+
+// else if(percent <=80 && percent >=70)
+// {
+//     alert("A")
+// }
+
+// else if(percent <=70 && percent >=60)
+// {
+//     alert("B")
+// }
+
+// else {
+//     alert("F")
+// }
+
+
+//      if ((c === d || x === y) && a === b) {
+//          h = g;
+//      }
+//          else {
+//          a = b;
+//          }
+
+// var students=["Ghous","Basit","Ali"]
+// console.log(students[1])
+
+// alert("welcome" + student[1])
+
+// var names=[];
+
+// names[0] ="ghous";
+// names[1]="basit";
+// console.log(names)
+
+
+// var students=["Ghous","Basit","Ali","Ghous","Basit","Ali"]
+// students.pop()
+
+// students.push("bilal")
+// alert(students)
+
+// var students=["Ghous","Basit","Ali"]
+// students.shift()
+
+// students.shift("bilal","zunaib")
+
+// students.splice(0,1,"bilal")
+
+//  var copystudents =students.slice(1,4)
+// console.log(students)
+// console.log(copystudents)
+
+
+// var x;
+// for(x=1; x<=10; x=x+2){
+//     document.write(x)
+// }
+
+// for(var i=0; i < 2; i++){
+//     alert("hello world!")
+// }
+
+// document.write("2" +"x"+ "1" +"=" + 2*1 +"<br")
+
+// for(var i=1 ; i <=10; i++){
+//     document.write("2" + "x" + i + "=" + 2*i +"<br>" )
+// }
+
+
+// var names=["hajra", "zoya", "mariam"];
+//     for(var i=0; i<names.length; i++){
+//         console.log(names[i])
+//     }
+
+
+
+// var cities=[,"gujranwala", "karachi","faisalabad","hyderabad"];
+// for(var i=0; i<cities.length; i++){
+//   if(cities[i]=== "hyderabad"){
+//     alert(cities[i]+ "is a cleanest city!")
+//   }
+// }
+
+
+// for(var i=1; i>0; i++){
+//     console.log("hello")
+// }
+
+
+// var c=a+b;
+// alert(c)
+// var a=20;
+// var b=10;
+// var c=a+b;
+// console.log(c)
+
+// var x= 20;
+// var y= "ali";
+// var z= y+z;
+//   alert(z);
+
+
+//  var x= 20;
+//   var y= "10";
+//   var z= y+x;
+//     alert(z);
+
+   
+//       var x= 20;
+//   var y= "2";
+//      var z= x*y;
+//        alert(z);
+
+//        var x= 20;
+//        var y= "2";
+//           var z= x/y
+//             alert(z);
+
+// var a=3
+// var b=7
+// var c=2
+// var d=(b-a) *c
+// alert(d);
+
+
+// var a=2
+// var b=a++;
+// alert(b);
+
+// alert(a);
+
+// var a=2
+// var b=++a;
+// alert(b);
+
+// var x=10
+// var y=x++ + ++x -x +x++-++x+x
+// alert(y);
+
+
+// var a=prompt("Enter your age","your age is");
+// alert(a);
+
+// var a=+prompt("Enter your value","your value is");
+// var b = a+45
+// console.log("The output is" , ""+b)
+
+// var spec=prompt("your species?","human");
+
+// var question = "Your species?";
+//  var defaultAnswer = "human";
+//  var spec = prompt(question, defaultAnswer);
+
+//  var numberOfhats = prompt("How many hats?");
+//  var tooManyhats =2 + 10;
+
+// var x="enter value";
+// var y="your value is"
+// var a=+prompt(x,y);
+// var b= a+10;
+// document.write("the output is" +"" +b)
+
+// var age=+prompt("enter your age");
+// if(age==14){
+//     alert("your age is equal")
+
+// }
+// else if(age <14){
+//     alert("your age is less"  )
+// }
+
+
+
+// else{
+//     alert("you age is more")
+// }
+
+// var age=+prompt("enter your age");
+// var gender=prompt("enter your gender")
+// if("age > 18 $$ gender==female"){
+//     alert("you are allowed")
+// }
+// else{
+//     alert("yoy are not allowed")
+// }
+
+// var a="5";
+// if (a==5){
+//     alert("true")
+
+// }
+// else{
+//     alert( "false")
+//        ( "false")
+//     }
+
+// var x=5;
+// var y=10;
+// vara=30;
+// var b=15;
+// if("x-y===a-b"){
+//     alert("correct")
+// }
+
+// else{
+//     alert("InCorrect")
+// }
+    
+// var a=5;
+// if(a !== 10){
+//     document.write("Correct")
+// }
+// else{
+
+// document.write("Incorrect")
+// }
+
+//  if (weight > 300 && time < 6) {
+//      alert("Come to our tryout!");
+//     }
+//      else {
+//      alert("Come to our cookout!");
+//      }
+
+//       if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//      alert("Come to our tryout!");
+//      }
+//      else {
+//          alert("Come to our cookout!");
+//          }
+
+// var percent= prompt("enter your percent");
+// if(percent >=80 && percent <=100){
+// alert("A+")
+// }
+
+// else if(percent <=80 && percent >=70)
+// {
+//     alert("A")
+// }
+
+// else if(percent <=70 && percent >=60)
+// {
+//     alert("B")
+// }
+
+// else {
+//     alert("F")
+// }
+
+
+//      if ((c === d || x === y) && a === b) {
+//          h = g;
+//      }
+//          else {
+//          a = b;
+//          }
+
+// var students=["Ghous","Basit","Ali"]
+// console.log(students[1])
+
+// alert("welcome" + student[1])
+
+// var names=[];
+
+// names[0] ="ghous";
+// names[1]="basit";
+// console.log(names)
+
+
+// var students=["Ghous","Basit","Ali","Ghous","Basit","Ali"]
+// students.pop()
+
+// students.push("bilal")
+// alert(students)
+
+// var students=["Ghous","Basit","Ali"]
+// students.shift()
+
+// students.shift("bilal","zunaib")
+
+// students.splice(0,1,"bilal")
+
+//  var copystudents =students.slice(1,4)
+// console.log(students)
+// console.log(copystudents)
+
+
+// var x;
+// for(x=1; x<=10; x=x+2){
+//     document.write(x)
+// }
+
+// for(var i=0; i < 2; i++){
+//     alert("hello world!")
+// }
+
+// document.write("2" +"x"+ "1" +"=" + 2*1 +"<br")
+
+// for(var i=1 ; i <=10; i++){
+//     document.write("2" + "x" + i + "=" + 2*i +"<br>" )
+// }
+
+
+// var names=["hajra", "zoya", "mariam"];
+//     for(var i=0; i<names.length; i++){
+//         console.log(names[i])
+//     }
+
+
+
+// var cities=[,"gujranwala", "karachi","faisalabad","hyderabad"];
+// for(var i=0; i<cities.length; i++){
+
+
+//   if(cities[i]=== "hyderabad"){
+
+  
+//     alert(cities[i]+ "is a cleanest city!")
+//     break;
+//   }
+// }
+
+  
+
+
+
+// for(var i=1; i>10; i++){
+//     if (i===10){
+//         break;
+//     }
+//     console.log("hello")
+// }
+
+// for(var a=1; a<=100 ;a = a+10){
+   
+//     for(var b=a; b < a+10;b++){
+//         document.write(b +"");
+
+//     }
+//    document.write("<br>")
+// }
+
+// var city=prompt("Enter your city name")
+// var firstchar =city.slice(0,1);
+// firstchar=firstchar.toUpperCase()
+// var otherchar=city.slice(1)
+// otherchar=otherchar.toLowerCase()
+
+
+
+// var city =firstchar + otherchar;
+// document.write(city)
+// var arr=['Karachi','Islamabad','Lahore']
+// for (var i=0; i<arr.length; i++){
+//     if (arr[i]===city){
+//         alert('City has found')
+
+//     }
+//     else{alert("City not found")}
+// }
+
+
+//  var str =prompt("Enter your text");
+//  var numChars = str.length;
+//  for (var i = 0; i < numChars; i++) {
+//  if (str.slice(i, i + 2) === "  ") {
+//  alert(" double spaces are found!");
+ 
+//  }
+//  }
+
+var text='The new Yorker magazine doesnt allow the phrase World War II  They say new should'
+//  for (var i = 0; i < text.length; i++) {
+//      if (text.slice(i, i + 12) === "World War II") {
+//      text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+//      console.log(text)
+//      }
+//      }
+
+//    var indexNum= text.lastIndexOf('new');
+//    var firstText= text.slice(0,indexNum)
+//    var replacingText= 'the second World War';
+//    var thirdText= text.slice(indexNum+12)
+
+// console.log(firstText+replacingText+thirdText);
+
+// var a="Javeriya";
+// undefined
+// console.log(a.charAt(6))
+// VM296:1 y
+// undefined
+// console.log(a.charAt(8))
+// VM309:1 
+// undefined
+// console.log(a.charAt(5))
+// VM322:1 i
+// undefined
+// console.log(a.charAt(0))
+// VM335:1 J
+
+
+// var para="this is text";
+// undefined
+// para1=para.replace("text","example");
+// 'this is example'
+// para1
+// 'this is example'
+
+
+
+// var num=1.5;
+// undefined
+// var round=Math.round(num);
+// undefined
+// round
+// 2
+// var num=1.3;
+// undefined
+// var round=Math.round(num);
+// undefined
+// round
+// 1
+// var num=2.5;
+// undefined
+// var round=Math.round(num)
+// undefined
+// round
+// 3
+// var num=1.5;
+// undefined
+// var round=Math.ceil(num);
+// undefined
+// Credential
+// ƒ Credential() { [native code] }
+// var num=2.5;
+// undefined
+// var round=Math.round(num)
+// undefined
+// round
+// 3
+// var num=1.6;
+// undefined
+// var round=Math.floor(num)
+// undefined
+// round
+// 1
+
+
+// Math.random()
+// 0.09024420508781672
+
+
+
+
+// var headsuser=prompt("Enter heads username");
+// var tailsuser=prompt("Enter tails username");
+// var toss=Math.random()*2;
+// var floor=Math.floor()
+// console.log(floor)
+// if(floor===0){
+//     alert("Heads",+headuser+"win the toss")
+
+// }
+
+// else{
+//     alert("Tails",+tailsuser+"win the toss")
+// }
+
+
+// var a=new Date();
+// undefined
+// a
+// Sat Jan 28 2023 22:31:10 GMT+0500 (Pakistan Standard Time)
+// a.toString()
+// 'Sat Jan 28 2023 22:31:10 GMT+0500 (Pakistan Standard Time)'
+// a
+// Sat Jan 28 2023 22:31:10 GMT+0500 (Pakistan Standard Time)
+// var b=a.toString();
+// undefined
+// var c=b.slice(0,3)
+// undefined
+// c
+// 'Sat'
+// a
+// Sat Jan 28 2023 22:31:10 GMT+0500 (Pakistan Standard Time)
+// a.getDay();
+// 6
+// a.getTime();
+// 1674927070661
+
+
+// a
+// Sat Jan 28 2023 22:31:10 GMT+0500 (Pakistan Standard Time)
+// a.getDay();
+// 6
+// a.getMonth();
+// 0
+// a.getSeconds();
+// 10
+// a.getHours();
+// 22
+// a.getfullYear();
+// VM595:1 Uncaught TypeError: a.getfullYear is not a function
+//     at <anonymous>:1:3
+// (anonymous) @ VM595:1
+// a.getFullYearullYear();
+// VM620:1 Uncaught TypeError: a.getFullYearullYear is not a function
+//     at <anonymous>:1:3
+// (anonymous) @ VM620:1
+// a.getFullYear();
+// 2023
+// a.getMinutes();
+// 31
+// a.getMilliseconds();
+// 661
+// var dob=new Date("sep 12, 2003);
+// VM834:1 Uncaught SyntaxError: Invalid or unexpected token
+// var Dob=new Date("sep 12, 2003);
+// VM846:1 Uncaught SyntaxError: Invalid or unexpected token
+// var dob=new Date("Sep 12, 2003");
+// undefined
+// var dobmili=dob.getTime();
+// undefined
+// var today=new date();
+// VM1009:1 Uncaught ReferenceError: date is not defined
+//     at <anonymous>:1:11
+// (anonymous) @ VM1009:1
+// var today=new Date();
+// undefined
+// var todaymili=today.getTime();
+// undefined
+// dobmili();
+// VM1127:1 Uncaught TypeError: dobmili is not a function
+//     at <anonymous>:1:1
+// (anonymous) @ VM1127:1
+
+// dobmili
+// 1063306800000
+// todaymili
+// 1674929148549
+// var diff=todaymili-dobmili
+// undefined
+// diff
+// 611622348549
+// var diffyear=diff/(1000*60*60*24*30*12)
+// undefined
+// diffyear
+// 19.663784354070216
+// var accuage=Math.floor(diffyear)
+// undefined
+// accuage
+// 19
+
+
+// function agecalc(){
+//  var dob=(newDateprompt("Enter your date of birth","sep 12 2003"))
+//  var dobmili=dob.getTime();
+//  var today=newDate();
+//  var todaymili=today.getTime();
+//  var diff=todaymili-dobmili;
+//  var accuage=Math.floor(diff(1000/60*60*24*30*12))
+//  document.write(accuage);
+
+// }
+// function greeting(){
+//     alert("Hello World")
+// }
+// greeting();
+
+
+// function greeting(greet){
+//     alert(greet)
+// }
+// greeting("Hello Aaqib");
+
+
+// function add(a,b,c){
+//     alert(a+b+c)
+    
+// }
+// add(5,8,1);
+
+// function add(a,b,c){
+//     var z=a+b+c
+//     return z;
+    
+// }
+// var k=add(5,8,1);
+// alert(k);
+
+
+// var h=19;
+// function add(a,b,c){
+//     alert(h);
+//          var z=a+b+c
+//          return z;
+// }
+
+// var k=add(5,8,1);
+// alert(k);
+
+
+
+// console.log("Hello!")
+// function first(greet){
+//     alert(greet)
+   
+
+
+
+
+// }
+
+// first("hello world!");
+// console.log("world!")
+
+
+// function first(b,a){
+//     alert(b+a)
+// }
+
+// first(1,3)
+
+
+// function foo(){
+//     var a="Javeriya";
+//     console.log(a);
+// }
+
+
+// var a="Javeriya"
+// function foo(){
+//     a ="Aayeza"
+// }
+
+// console.log(a);
+
+
+// function calc(num1,opr,num2){
+//     if(opr==="+"){
+//         return(num1+num2)
+//     }
+//     else if(opr==="-"){
+//         return(num1-num2)
+//     }
+//     else if(opr==="*"){
+//         return(num1*num2)
+        
+//     }
+//     else("incorrect operator!")
+// }var result =calc(5,"-",2);
+// var result1=calc(5,"+",6);
+// var result2=calc(7,"%",7);
+// console.log(result)
+// console.log(result1)
+// console.log(result2)
+
+
+//  function foo(){
+
+// }
+
+// console.log(foo())
+
+
+// var name="Javeriya"
+// switch(name){
+//     case "Javeriya":
+//     alert("hello javeriya!")
+//     break;
+//     case "Aaqib":
+//     alert("hello Aaqib!")
+//     break;
+//     default:
+//         alert("wrong")
+// }
+
+
+// for(var a=10 ;a<10; a++){
+// console.log(a)
+// } (how we write for loop)
+
+
+// var a=0;
+// while(a<10){
+//     console.log(a);
+//     a++;
+// }
+
+
+//  var i = 0;
+//  do {
+//  alert(i);
+//  i++;
+//  } while (i < 0);
+
+
+// function foo(greet){
+//     alert(greet)
+// }
+
+
+// function clickbtn(){
+//     alert("click");
+// }
+
+
+// function getname(){
+//     var name=document.getElementById('name');
+//     console.log(name.value)
+
+//     name.value=""
+
+// }
+
+
+// function setname(){
+//     var name=document.getElementById('name');
+//   name.value="Javeriya";
+//   var para=document.getElementById("para");
+//   para. innerHTML="this is a paragrph."
+// }
+// setname()
+
+
+// function getnumber(num){
+ 
+//   var result =document.getElementById("result");
+
+//   result.value +=num;
+// }
+// function clearResult(){
+//   var result =document.getElementById("result");
+//    result.value=""
+// }
+
+// function getResult(){
+//   var result =document.getElementById("result");
+//  result.value=eval(result.value)
+// }
+
+// function readmore(){
+// var text='<ul><li>list1</li><li>list1</li><li>list1</li><li>list1</li></ul>'
+//   var para=document.getElementById('para')
+//    para.innerHTML=text
+// }
+
+// function big(){
+//   var img=document.getElementById('car')
+//   img .className += ' hidden'
+// }
+
+// function changeImage(){
+//   var carImg=document.getElementById('img')
+//   carImg.src ='https://i.ytimg.com/vi/tArC9-RHmU4/maxresdefault.jpg'
+// }
+  
+// function changeImageBefore(){
+//   var carImg=document.getElementById('img')
+//   carImg.src='https://imageio.forbes.com/specials-images/imageserve/5f962df3991e5636a2f68758/0x0.jpg?format=jpg&crop=812,457,x89,y103,safe&width=1200'
+// }
+
+// function changeImage(){
+//   var image= document.getElementById('para');
+//   image.style.fontSize='200%'
+
+// }
+
+//    var para= document.getElementsByTagName('p');
+//  para[3].style.color='red'
+//  for (var i=0;i<para.length;i++)
+//  para[i].style.color='purple'
+
+ 
+//  var parent =  document.getElementsById('parent')
+//  var para = parent.getElementsByTagName('p');
+
+//  for (var i=0;i<para.length;i++){
+
+ 
+//  para[i].style.color='orange'
+
+//  }
+
+// function bigImage(){
+//     var image=document.getElementById('text');
+//     image.className="big"
+// }
+
+// function smallImage(){
+//     var image=document.getElementById('text');
+//     image.className="small"
+// }
+
+// function addClass(){
+//     var para=document.getElementById('small');
+//     para.className +=" bold class"
+// }
+
+// function getElements(){
+//     var element=document.getElementsByTagName("p")
+//     console.log(element)
+// }
+//  getElements()
+
+// var element=document.getElementById('parent')
+//  var element=document.getElementsByTagName('p')
+//  for(var i=0; i<para.length; i++){
+//     para[1].style.color='red'
+//  }
+
+
+//  var count=0;
+// var interval;
+// function timer(){
+//     count++
+//     console.log("count")
+// }
+
+
+// interval=setInterval(timer,1000)
+
+// setTimeout (function()
+// {
+//     clearInterval(interval)
+// },5000)
+
+
+
+// function timeout(){
+//     console.log("running")
+// }
+
+// setTimeout(timeout,1000)
+
+
+console.log(document.childNodes[1].childNodes[0])
+
+
+
+
+
+
+
+
+//objects in JS//
+
+
+
+
+
+//    var myQuestion=[
+//     {
+//         question : "what is your name",
+//         answer:{
+//             a:"ali",
+//             b:"basit",
+//             c:"ghous"
+//         },
+
+//         correctAnswer:"c"
+//     },
+
+ 
+    
+//         {
+//             question : "what is your age",
+//             answer:{
+//                 a:"12",
+//                 b:"23",
+//                 c:"24"
+//             },
+    
+//             correctAnswer:"b"
+//         },
+    
+
+//    ]
+
+
+
+   //object walay chps ka kaam
+
+
+//    var student1 ={
+//     name:"ghous",
+//     rollno:"123",
+//    }
+
+
+   
+   // var student2 ={
+   //  name:"ghazanfar",
+   //  rollno:"122",
+   //  school:"abc",
+   // }
+
+   // console.log(student2)
+
+//    console.log(student2.hasOwnProperty("rollno"))
+
+//   for(var prop in student2){
+//     console.log(prop,student2[prop])
+//   }
+
+
+
+
+//constructor function
+
+
+// function Student(name,rollno){
+//     this.name=name;
+//     this.rollno=rollno
+// }
+
+//prototype 
+
+// Student.prototype.getName= function(){
+//     return this.name
+// }
+
+//     this.getname = function(){
+//         return this.name;
+//     }
+
+
+// var student1 = new Student("ghous",123);
+// var student2 = new Student("ghazanfar",122);
+
+// console.log(student1)
+// console.log(student2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
